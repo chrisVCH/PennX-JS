158cf969418556b530f442d0eb485b3e41186244
Geronimo Gil <gilgeronimo0@gmail.com>
2017-10-07 21:47:50 -0300

    copy course example and run at node server

--- Week3/ej7.dogsList/dogslistapp/src/App.js
+++ Week3/ej7.dogsList/dogslistapp/src/App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Dogs from './components/Dogs';
+import AddDog from './components/AddDog';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dogs: []
+    };
+  }
+
+  getDogs() {
+    var defaultDogs = {dogs: [
+      {
+        name: 'Princess',
+        breed: 'Corgi',
+        image: 'https://s-media-cache-ak0.pinimg.com/originals/51/ae/30/51ae30b78696b33a64661fa3ac205b3b.jpg'
+      },
+      {
+        name: 'Riley',
+        breed: 'Husky',
+        image: 'http://portland.ohsohandy.com/images/uploads/93796/m/nice-and-sweet-siberian-husky-puppies-for-free-adoption.jpg'
+      },
+    ]};
+    this.setState(defaultDogs);
+  }
+
+  componentWillMount() {
+    this.getDogs();
+  }
+
+  handleAddDog(dog) {
+    let dogs = this.state.dogs;
+    dogs.push(dog);
+    this.setState({dogs:dogs});
+  }
+
+  handleDeleteDog(name) {
+    let dogs = this.state.dogs;
+    let index = dogs.findIndex(x => x.name === name);
+    dogs.splice(index, 1);
+    this.setState({dogs:dogs});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dogs dogs={this.state.dogs} onDelete={this.handleDeleteDog.bind(this)} />
+        <AddDog addDog={this.handleAddDog.bind(this)} />
+        <hr />
       </div>
     );
   }
