e77238aa2f558e8bfa3605d33e077f3b3d64841a
Geronimo Gil <gilgeronimo0@gmail.com>
2017-10-14 18:16:29 -0300

    request and response objects and methods

--- Week4/ej1.helloWorld/index.js
+++ Week4/ej1.helloWorld/index.js
@@ -2,7 +2,18 @@ var express = require('express');
 var app = express();
 
 app.use('/', (req, res) => {
-  res.send('Hello World!');
+
+  var method = req.method;
+  var url = req.url;
+  var headers = req.headers;
+
+  res.status(200);
+  res.type('html');
+  res.write('Hello World');
+  res.write('<p>');
+  res.write('method: ' + method + ' url: ' + url + ' headers: ' + headers);
+
+  res.end();
 });
 
 app.listen(3000, () => {
