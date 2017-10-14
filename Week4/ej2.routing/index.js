41161631e00e1b7037f79e5e2884a299e2bbc4d6
Geronimo Gil <gilgeronimo0@gmail.com>
2017-10-14 19:04:40 -0300

    express router example. commbines multiple middleware functions into one common sub-route

--- Week4/ej2.routing/index.js
+++ Week4/ej2.routing/index.js
@@ -23,23 +23,37 @@ var greeter = (req, res, next) => {
   next();
 }
 // another middleware function
-var adminName = (req,res,next) => {
+var adminName = (req, res, next) => {
   req.username = 'Admin';
+  next();
+}
+
+var logger = (req, res, next) => {
+  var url = req.url;
+  var time = new Date();
+  console.log('Recieved for ' + url + ' at ' + time);
+
   next();
 }
 
 // now we can use multiple middleware in a single route
 // this is called 'chaining'
-app.use('/welcome', nameFinder, greeter, (req, res) => {
+app.use('/welcome', nameFinder, greeter, logger, (req, res) => {
   res.end();
 });
 
 // and in another route, another chain
-// we chaing 3 middleware functions here
-app.use('/admin', adminName, greeter, (req, res) => {
+// we chaing 4 middleware functions here
+app.use('/admin', adminName, greeter, logger, (req, res) => {
   res.end();
 });
 
+// Router example
+// we can combine middleware function in common sub-routes
+var commonRoute = express.Router();
+commonRoute.use(greeter, logger, (req, res) => {res.end();});
+
+app.use('/router', adminName, commonRoute);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
