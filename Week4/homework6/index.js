6db7374e52da01fb935a546682d3ed9b623398b7
Geronimo Gil <gilgeronimo0@gmail.com>
2017-10-17 20:34:55 -0300

    homework 6 requirements

--- Week4/homework6/index.js
+++ Week4/homework6/index.js
@@ -9,10 +9,33 @@ app.use('/', (req, res) => {
 	res.json({ msg : 'It works!' });
     });
 
+// finds toy with a given id
+// query: /findToy?id=1234
+app.use('/findToy', (req,res) => {
+
+});
+
+// finds animals
+// query /findAnimals?species=Dog&trait=loyal&gender=female
+app.use('/findAnimals', (req,res) => {
+
+});
+
+// finds animals under a certain age
+// query /animalsYoungerThan?age=12
+app.use('/animalsYoungerThan', (req,res) => {
+
+});
+
+// calculates price of a sum of toys
+// query /calculatePrice?id[0]=123&qty[0]=2&id[1]=456&qty[1]=3
+app.use('/calculatePrice', (req,res) => {
+
+});
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
-    });
+});
 
 
 
