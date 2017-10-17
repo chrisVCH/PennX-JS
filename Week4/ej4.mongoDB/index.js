f12727205cf0952f443cc91f97ec0d96bed44451
Geronimo Gil <gilgeronimo0@gmail.com>
2017-10-17 19:40:27 -0300

    query all entries and a specific one

--- Week4/ej4.mongoDB/index.js
+++ Week4/ej4.mongoDB/index.js
@@ -9,6 +9,7 @@ var Person = require('./schemas/Person.js');
 
 app.use('/public', express.static('files'));
 
+// create new entry and save to DB
 app.use('/create', (req, res) => {
   // create new person, from Person.js, passing in values
   var newPerson = new Person( {
@@ -27,6 +28,38 @@ app.use('/create', (req, res) => {
   })
 });
 
+// query all entries in DB
+app.use('/all', (req, res) => {
+  // use person schema
+  Person.find( (err, allPeople) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else if (allPeople.length === 0) {
+      res.type('html').status(200);
+      res.send('There are no people!')
+    } else {
+      res.send(allPeople);
+    }
+  })
+})
+
+// query one entry in DB
+app.use('/person', (req, res) => {
+  var searchName = req.query.name;
+  // Schema.findOne(queryString, callback)
+  Person.findOne( {name: searchName}, (err, person) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else if (person.length === 0) {
+      res.type('html').status(200);
+      res.send('No person named ' + searchName);
+    } else {
+      res.send(person);
+    }
+  })
+})
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
