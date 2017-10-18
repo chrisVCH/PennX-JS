3239ead08f16f44c195cd904b4ff299bed6bb912
Geronimo Gil <gilgeronimo0@gmail.com>
2017-10-18 18:34:01 -0300

    createToy and createAnimal for debugging

--- Week4/homework6/index.js
+++ Week4/homework6/index.js
@@ -4,10 +4,56 @@ var app = express();
 var Animal = require('./Animal.js');
 var Toy = require('./Toy.js');
 
-
+/*
 app.use('/', (req, res) => {
 	res.json({ msg : 'It works!' });
-    });
+});
+*/
+
+// create new entry and save to DB
+// http://localhost:3000/createToy?id=1234&name=toy1&price=2.2
+app.use('/createToy', (req, res) => {
+
+	var newToy = new Toy( {
+		id: req.query.id,
+		name: req.query.name,
+		price: req.query.price,
+	});
+
+  newToy.save( (err) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else {
+      res.type('html').status(200);
+      res.json(1);
+    }
+  })
+});
+
+// create new entry and save to DB
+// http://localhost:3000/createAnimal?name=tt&species=dog&breed=yorkie&gender=female&traits=crazy&age=3
+app.use('/createAnimal', (req, res) => {
+
+	var newAnimal = new Animal( {
+		name: req.query.name,
+		species: req.query.species,
+		breed: req.query.breed,
+		gender: req.query.gender,
+		traits: req.query.traits,
+		age: req.query.age,
+	});
+
+  newAnimal.save( (err) => {
+    if (err) {
+      res.type('html').status(500);
+      res.send('Error: ' + err);
+    } else {
+      res.type('html').status(200);
+      res.json(1);
+    }
+  })
+});
 
 // finds toy with a given id
 // query: /findToy?id=1234
